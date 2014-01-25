32c0db7bac0acab60fbc85795d31ebec93fff26b
Joe Mercer <mynameisjoemercer@gmail.com>
2014-01-25 15:17:08 -0500

    use easy to use fb oauth config instead of doing it in file

--- server/accounts.js
+++ server/accounts.js
@@ -1,12 +1,3 @@
-Accounts.loginServiceConfiguration.remove({
-    service: "facebook"
-});
-Accounts.loginServiceConfiguration.insert({
-    service: "facebook",
-    appId: "607442322661481",
-    secret: "7731fdbd9a85748fc2e16038f8cfebe6"
-});
-
 Accounts.onCreateUser(function(options, user) {
     if (options.profile) {
         options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture?type=large";
