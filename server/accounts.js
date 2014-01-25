7998d51d200a84e722604f51a95b54dc0f379296
bonnieli <bonnielokli@gmail.com>
2014-01-25 14:32:59 -0500

    accounts

--- server/accounts.js
+++ server/accounts.js
@@ -1,3 +1,12 @@
+Accounts.loginServiceConfiguration.remove({
+    service: "facebook"
+});
+Accounts.loginServiceConfiguration.insert({
+    service: "facebook",
+    appId: "1459026397651152",
+    secret: "53232bf57d941c28c5816e165890af6a"
+});
+
 Accounts.onCreateUser(function(options, user) {
     if (options.profile) {
         options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture?type=larger";
